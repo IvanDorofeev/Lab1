92af7e20b822b80a61188b784428adbba21b6870
Иван Дорофеев <dfyz.20032020@gmail.com>
2023-10-11 18:18:47 +0300

    Lab1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Container<Integer> list = new Container<Integer>();
-        System.out.println(list.length());
+        System.out.println("Длина списка: " + list.length());
         for (int i = 0; i < 10; i++) {
             list.add_begin(i);
         }
@@ -11,14 +11,14 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             list1.add_begin(i);
         }
-        System.out.println(list.equals(list1));
-        System.out.println(list.length());
+        System.out.println("Оба списка идентичны: " + list.equals(list1));
+        System.out.println("длина 1-го: " + list.length());
         int a = list.pop(5);
-        System.out.println(list.length());
-        System.out.println(a);
+        System.out.println("Длина 1-го после удаления: " + list.length());
+        System.out.println("Удаленный элемент: " + a + "\nсписок: ");
         list.print();
-        System.out.println(list);
+        System.out.println("1-ый список после удаления: " + list);
         list.add_last(8);
-        System.out.println(list.index_of(8));
+        System.out.println("Индекс 8-ки: " + list.index_of(8));
     }
 }
